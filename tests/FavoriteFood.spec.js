2b3e8630caa2f3b7c5f0a9ebf40d6802caefeb4f
Caleb Faught <faughtcj@gmail.com>
2017-01-02 18:38:57 -0600

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,5 +1,11 @@
 describe('Favorite Food Filter', function () {
 	var $controller;
+	var people = [
+		{name: 'bob', favoriteFood: 'pizza', id: 1},
+		{name: 'bill', favoriteFood: 'tacos', id: 2},
+		{name: 'beau', favoriteFood: 'pasta', id: 3},
+		{name: 'lloyd', favoriteFood: 'sushi', id: 4}
+	]
 
 	beforeEach(module('app'));
 
@@ -7,5 +13,11 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	beforeEach(inject(function($filter) {
+		favoriteFood = $filter('favoriteFood')
+	}));
+
+	it('should return only favorite foods', function() {
+		expect(favoriteFood(people, "pizza")[0].id).toBe(1);
+	})
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,15 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	beforeEach(inject(function($filter) {
+		removeAllVowels = $filter('removeAllVowels');
+	}));
+
+	it('should remove vowels from word', function() {
+		expect(removeAllVowels('hello')).toEqual('hll');
+	});
+
+	it('should remove all vowels from sentence', function() {
+		expect(removeAllVowels('Well, hello there Adam!')).toEqual('Wll, hll thr dm!');
+	});
 });
